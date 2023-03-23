ec329625aa2c5fff4f63b6eca2adabcd793d76b0
Benjamin Cowley <benthe.blueturkey@gmail.com>
2023-03-23 17:53:22 -0600

    Remove delve changes from branch

--- src/actors/delve/sheet.js
+++ src/actors/delve/sheet.js
@@ -1,17 +1,9 @@
 import sheetHTML from './sheet.html';
-import './delve.sass';
 import HeartActorSheet from '../base/sheet';
 import template from './template.json';
 
 export default class DelveSheet extends HeartActorSheet {
     static get type() { return Object.keys(template.Actor)[0]; }
-
-    getData() {
-      const data = super.getData();
-      data.user = game.user;
-      data.showTextboxesBelowItems = game.settings.get('heart', 'showTextboxesBelowItems')
-      return data;
-    }
     
     get template() {
         return sheetHTML.path;
