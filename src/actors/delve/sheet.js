f7c3acaa50d33a328fbb43387b3f3bfebbc4c81e
Benjamin Cowley <benthe.blueturkey@gmail.com>
2023-03-23 15:24:14 -0600

    Adding functionality to delve sheet for various properties

--- src/actors/delve/sheet.js
+++ src/actors/delve/sheet.js
@@ -5,6 +5,13 @@ import template from './template.json';
 
 export default class DelveSheet extends HeartActorSheet {
     static get type() { return Object.keys(template.Actor)[0]; }
+
+    getData() {
+      const data = super.getData();
+      data.user = game.user;
+      data.showTextboxesBelowItems = game.settings.get('heart', 'showTextboxesBelowItems')
+      return data;
+    }
     
     get template() {
         return sheetHTML.path;
