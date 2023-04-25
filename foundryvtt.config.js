46b15a6b79fe368ea43033f59fb1983a2429306f
Benjamin Cowley <benthe.blueturkey@gmail.com>
2023-04-25 16:55:34 -0600

    Add foundry.config back

---
+++ foundryvtt.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    type: 'system',
+    name: 'heart',
+    version: '0.7.0',
+    authors: ['hitcherland'],
+    title: 'Heart - The City Beneath',
+    description: 'A red wet heaven slumbers fitful under the city of Spire. Purchase at https://rowanrookanddecard.com/product/heart-the-city-beneath-rpg/',
+    compatibility: {
+        "minimum": "10",
+        "verified": "10"
+    },
+    githubRepo: "hitcherland/foundryvtt-heart",
+    githubBranch: "release",
+    foundryvttPath: "/home/bcowley/foundryuserdata/Data",
+};
